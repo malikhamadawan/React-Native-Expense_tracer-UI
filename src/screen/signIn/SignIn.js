a67dace6ab6a9a37e0d2c79959b54943b67200c0
malikhamadawan <hamadawan145@gmail.com>
2022-04-26 02:46:09 +0500

    Sign in screen start

--- src/screen/signIn/SignIn.js
+++ src/screen/signIn/SignIn.js
@@ -1,27 +1,84 @@
 import React from 'react';
-import {View,Dimensions,Text} from 'react-native';
-const {height,width} =Dimensions.get('window');
+import { View, Dimensions, Text, Image, ScrollView, TextInput,TouchableOpacity } from 'react-native';
+const { height, width } = Dimensions.get('window');
 import Styles from '../styles/Styles';
 import LinearGradient from 'react-native-linear-gradient';
-export const SignIn =()=>{
-    return(
+import Icons from 'react-native-vector-icons/Ionicons'
+import { moderateScale } from '../../../Theme/Dimensions';
+export const SignIn = () => {
+    return (
         <View style={Styles.container}>
-            <LinearGradient
-            start={{x:0,y:0}}
-            end={{x:1,y:1}}
-            colors={['#178BE3FF','#B054CBFF','#E98B42FF']}
-            style={Styles.linearGradient}
-            >
-                <View style={Styles.linearView}>
-                    <Text
-                    style={{
-                        fontSize: 45,
-                        color: 'white',
-                        fontFamily: "WaterBrush-Regular",
-                    }}
-                    >Expense Tracer</Text>
-                </View>
-            </LinearGradient>
+            <ScrollView>
+                <LinearGradient
+                    start={{ x: 0, y: 0 }}
+                    end={{ x: 1, y: 0.3 }}
+                    colors={['#178BE3FF', '#B054CBFF', '#E98B42FF']}
+                >
+                    <View style={Styles.linearView}>
+                        <Text
+                            style={{
+                                fontSize: 65,
+                                color: 'white',
+                                fontFamily: 'MsMadi-Regular',
+                            }}
+                        >Expense Tracer</Text>
+                    </View>
+                    <View style={Styles.view} >
+                        {/* view2 */}
+                        <View style={Styles.view2} >
+                            <Text style={Styles.text}>SIGN IN</Text>
+                        </View>
+                        {/* view3 */}
+                        <View style={Styles.view3} >
+                            {/* view4 */}
+                            <View
+                                style={Styles.userView}
+                            >
+                                <View style={Styles.view4}>
+                                    {/* innerView */}
+                                    <View style={Styles.innerView} >
+                                        <Icons name={'ios-person'} color={'black'} size={30} />
+                                    </View>
+                                    <TextInput placeholder='User name' placeholderTextColor={'grey'} />
+                                </View>
+                                <View style={Styles.view4}>
+                                    {/* innerView */}
+                                    <View style={Styles.innerView} >
+                                        <Icons name={'ios-lock-closed'} color={'black'} size={30} />
+                                    </View>
+                                    <TextInput placeholder='Password' placeholderTextColor={'grey'} />
+                                </View>
+                                <Text
+                                    style={{
+                                        fontSize: 16,
+                                        marginLeft: moderateScale(220),
+                                        color: 'black',
+                                    }}
+                                >Forgot Password..?</Text>
+                            </View>
+
+                            <TouchableOpacity style={Styles.buttonContainer}  >
+                                <LinearGradient
+                                    start={{ x: 0, y: 0 }}
+                                    end={{ x: 1, y: 1 }}
+                                    colors={['#178BE3FF', '#B054CBFF', '#E98B42FF']}
+                                    style={Styles.linearGradient}
+                                >
+                                    <View style={Styles.button}>
+
+
+
+                                        <Text style={Styles.text2} >SIGN IN</Text>
+
+                                    </View>
+                                </LinearGradient>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </LinearGradient>
+            </ScrollView>
+
+
         </View>
     );
 }
--- src/screen/styles/Styles.js
+++ src/screen/styles/Styles.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Dimensions, StyleSheet } from 'react-native';
+import { moderateScale } from '../../../Theme/Dimensions';
+import Theme from '../../../Theme/Theme';
 const { height, width } = Dimensions.get('window');
 const Styles = StyleSheet.create({
     mainContainer: {
@@ -12,13 +14,82 @@ const Styles = StyleSheet.create({
         flex: 1,
         backgroundColor: 'white',
     },
-    linearGradient:{
-        padding: 15,
+    linearGradient: {
+        borderRadius: 30,
+        elevation: 5,
     },
     linearView: {
-        height: (height/100)*30,
+        height: (height / 100) * 30,
         alignItems: 'center',
         justifyContent: 'center',
-    }
+    },
+    view: {
+        height: (height / 100) * 55,
+        width: (width / 100) * 100,
+        backgroundColor: 'white',
+        borderTopLeftRadius: 35,
+        borderTopRightRadius: 35,
+        overflow: 'hidden',
+    },
+    view2: {
+        height: (height / 100) * 12,
+        //  backgroundColor: 'red',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    view3: {
+        height: (height / 100) * 35,
+        // backgroundColor: 'red',
+
+    },
+
+    view4: {
+        height: moderateScale(50),
+        width: (width / 100) * 95,
+        backgroundColor: 'white',
+        borderRadius: 30,
+        overflow: 'hidden',
+        flexDirection: 'row',
+        elevation: 5
+    },
+    innerView: {
+        height: moderateScale(50),
+        width: moderateScale(70),
+        // backgroundColor: 'blue',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    userView: {
+        height: (height / 100) * 25,
+        // backgroundColor: 'blue',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+    },
+    text: {
+        fontSize: Theme.fontSizes.xbig,
+        color: 'black',
+
+    },
+    text2: {
+        fontSize: Theme.fontSizes.xmedium,
+        color: 'white',
+
+    },
+    buttonContainer: {
+        height: (height / 100) * 10,
+        width: (width / 100) * 100,
+        // backgroundColor: 'green',
+        alignItems: 'center',
+        justifyContent: 'center',
+        overflow: 'hidden',
+    },
+    button: {
+        height: moderateScale(50),
+        width: moderateScale(360),
+        alignItems: 'center',
+        justifyContent: 'center',
+        overflow: 'hidden',
+    },
+
 });
 export default Styles
